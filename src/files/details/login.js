45521e11a61424ef2ec91e35a8125ca8904fdcdd
srazzzz <sravs.vadlamanu@gmail.com>
2022-12-14 02:08:47 +0530

    added login form validation

--- src/files/details/login.js
+++ src/files/details/login.js
@@ -1,32 +1,72 @@
 import React from 'react';
-
+import { useState } from "react";
 import "./login.css"
-function LoginPage(){
+function LoginPage() {
+    const initialValues = {idNumber: "" , password:""}
+    const [formErrors, setFormErrors] = useState("")
+    const [formValues, setFormValues] = useState(initialValues)
+
+    const handleInput = (event) =>
+    {
+        const {name , value} = event.target;
+        setFormValues({...formValues , [name]: value});
+    }
+
+    const validate = (formValues) => {
+		console.log(formErrors,formValues)
+		const errors = {}
+        if (!formValues.idNumber) {
+
+			errors.idNumber = "user name required!"
+		}
+        if (!formValues.password) {
+			errors.password = "Password is required";
+		} else if (formValues.password.length < 4) {
+			errors.password = "Password must be more than 4 characters";
+		} else if (formValues.password.length > 10) {
+			errors.password = "Password cannot exceed more than 10 characters";
+		}
+        else if(formValues.password !== formValues.idNumber){
+			errors.password = "password not matching ";
+		}
+		return errors;
+	}
+
+    const handleLogin = (event) => {
+		setFormErrors(validate(formValues))
+        console.log(validate(formValues))
+		event.preventDefault()
+		if(validate(formValues).length === 0){		//not working
+			setFormValues(initialValues)
+    
+		}
+	}
     return (
         <>
-        <div className="whole-page">
-        <div class="login-page">
-        <div class="form">
-            {/* register form  */}
-            <h1>LOGIN</h1>
-            <form class="register-form">
-                <input type="text" placeholder="name" />
-                <input type="password" placeholder="password" />
-                <input type="text" placeholder="email address" />
-                <button>create</button>
-                <p class="message">Already registered? <a href="a">Sign In</a></p>
-                {/* here we have to add the library page afetr login  */}
-            </form>
-            {/* if already avean account login redirecting  */}
-            <form class="login-form">
-                <input type="text" placeholder="username" />
-                <input type="password" placeholder="password" />
-                <button className= "buttonlogin " onclick="location.href=librarypage.html">login</button>
-                <br/><br/>
-                <p class="message">Not registered? <a href="register.html">Create an account</a></p>
-            </form>
-        </div>
-        </div></div>
+            <div className="whole-page">
+                <div class="login-page">
+                    <div class="form">
+                        {/* register form  */}
+                        <h1>LOGIN</h1>
+                        <br/>
+                        {/* if already avean account login redirecting  */}
+                        <form class="login-form">
+                            <div className="form-control">
+                                <label htmlFor="username">ID Number</label>
+                                <input type="text" id="idNumber" name="idNumber" placeholder="Enter college ID"  onChange={handleInput} />
+                                <div className="error">{formErrors.idNumber}</div>
+                            </div>
+                            <div className="form-control">
+                                <label htmlFor="password">Password</label>
+                                <input type="password" id="password" name="password" placeholder="Enter password" onChange={handleInput}  />
+                                <div className="error">{formErrors.password}</div>
+                            </div>
+                            <div className="buttonlogin"><button className="buttonlogin"  type="submit" onClick={handleLogin}>submit</button></div>
+                           
+                            <p class="message">Not registered? <a href="register.html">Create an account</a></p>
+                        </form>
+                    </div>
+                </div></div>
         </>
     )
 }
